150946d6cd5243f68e16a01e0cd8a5d1ec1c4891
Dilyana Gineva <dilyana_gineva@abv.bg>
2018-03-20 16:01:41 +0200

    Data classes and Video Controller methods in progress

--- src/controllers/VideoController.java
+++ src/controllers/VideoController.java
@@ -1,5 +1,67 @@
 package controllers;
 
+import datastorage.Comment;
+import datastorage.User;
+
 public class VideoController {
 
+	public static void addComment(String comment, User user) {
+
+	}
+
+	public static void removeComment(Comment comment) {
+
+	}
+
+	public static void addLikeToComment(Comment comment) {
+
+	}
+
+	public static void removeLikeFromComment(Comment comment) {
+
+	}
+
+	public static void addDislikeToComment(Comment comment) {
+
+	}
+
+	public static void removeDislikeFromComment(Comment comment) {
+
+	}
+
+	public static void addResponseToComment(String comment, User user) {
+
+	}
+
+	public static void removeResponseFromComment(Comment comment) {
+
+	}
+	
+	public static void addLikeToVideo() {
+
+	}
+
+	public static void removeLikeFromVideo() {
+
+	}
+
+	public static void addDislikeToVideo() {
+
+	}
+
+	public static void removeDislikeFromVideo() {
+
+	}
+
+	public static void addVideoToPlaylist(String name) {
+
+	}
+
+	public static void removeVideoFromPlaylist(String name) {
+
+	}
+
+	public static void followChannel() {
+
+	}
 }
--- src/datastorage/Comment.java
+++ src/datastorage/Comment.java
@@ -1,5 +1,40 @@
 package datastorage;
 
-public class Comment {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+import exceptions.IllegalChannelArgumentException;
+import exceptions.IllegalCommentContentException;
+
+public class Comment {
+	private static final int DEFAULT_DISLIKES_TO_COMMENTS = 0;
+	private static final int DEFAULT_LIKES_TO_COMMENTS = 0;
+	
+	private final Channel channel;
+	private Date publicationDate;
+	private long likes;
+	private long dislikes;
+	private List<Comment> responses;
+	private String content;
+	
+	public Comment(Channel channel, String content) throws IllegalChannelArgumentException, IllegalCommentContentException{
+		if(channel != null){
+			this.channel = channel;
+		}else{
+			throw new IllegalChannelArgumentException();
+		}
+		
+		if(content != null && content.trim().length() > 0){
+			this.content = content;
+		}else{
+			throw new IllegalCommentContentException();
+		}
+		
+		this.publicationDate = new Date();
+		this.likes = DEFAULT_LIKES_TO_COMMENTS;
+		this.dislikes = DEFAULT_DISLIKES_TO_COMMENTS;
+		
+		this.responses = new ArrayList<Comment>();
+	}
 }
